7eb1305036b9e5da9688fa95f5025dddf787e8ab
Szymon Grabowski <szmiko@wp.pl>
2018-06-11 22:00:57 +0200

    Task

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,71 @@
 const path = require('path');
 
-module.exports = {
-	entry: './src/index.js',
-	output: {
-		path: path.resolve(__dirname, 'build'),
-		filename: 'app.bundle.js'
-	},
-	module: {
-		rules: [
-			{
-				test: /\.js$/,
-				loader: "babel-loader"
-			},
-			{
-				test: /\.css$/,
-				use: [
-					{ loader: 'style-loader'},
-					{
-						loader: 'css-loader',
-						options: {
-							modules: true
+module.exports = (env) => {
+	const environment = env;
+	return {
+		mode: 'production',
+		entry: './src/index.js',
+		output: {
+			path: path.resolve(__dirname, 'build'),
+			filename: 'app.' + production + 'bundle.js'
+		},
+		optimization: {
+			minimize: false
+		},
+		module: {
+			rules: [
+				{
+					test: /\.js$/,
+					loader: "babel-loader"
+				},
+				{
+					test: /\.css$/,
+					use: [
+						{ loader: 'style-loader'},
+						{
+							loader: 'css-loader',
+							options: {
+								modules: true
+							}
 						}
-					}
-				]
-			}
-		]
-	}
+					]
+				}
+			]
+		}
+	};
+};
+
+module.exports = (mode) => {
+	const environment = mode;
+	return {
+		mode: 'development',
+		entry: './src/index.js',
+		output: {
+			path: path.resolve(__dirname, 'build'),
+			filename: 'app.' + development + 'bundle.js'
+		},
+		optimization: {
+			minimize: false
+		},
+		module: {
+			rules: [
+				{
+					test: /\.js$/,
+					loader: "babel-loader"
+				},
+				{
+					test: /\.css$/,
+					use: [
+						{ loader: 'style-loader'},
+						{
+							loader: 'css-loader',
+							options: {
+								modules: true
+							}
+						}
+					]
+				}
+			]
+		}
+	};
 };
